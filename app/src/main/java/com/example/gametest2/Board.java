adae5b992218cf0c3eb9a5f6317fa8768b7307f4
freezy <abhiakkal27@gmail.com>
2023-03-21 09:28:37 -0700

    All action methods complete

--- app/src/main/java/com/example/gametest2/Board.java
+++ app/src/main/java/com/example/gametest2/Board.java
@@ -7,7 +7,7 @@ import android.util.AttributeSet;
 public class Board extends GameView{
     //here is the board class for the basic-basic make of the game
     Paint red = new Paint();
-    Paint blue = new Paint();
+    Paint lblue = new Paint();
     Paint black = new Paint();
     Paint white = new Paint();
     Paint dBlue = new Paint();
@@ -21,6 +21,17 @@ public class Board extends GameView{
         gm = new GameModel();
 
         red.setColor(0xFFFF0000);
+        red.setStyle(Paint.Style.FILL);
+        lblue.setColor(0xFF1FBED6);
+        lblue.setStyle(Paint.Style.FILL);
+        black.setColor(0xFF000000);
+        black.setStyle(Paint.Style.FILL);
+        white.setColor(0xFFFFFFFF);
+        white.setStyle(Paint.Style.FILL);
+        dBlue.setColor(0xFF84B1D0);
+        dBlue.setStyle(Paint.Style.FILL);
+        pink.setColor(0xFFFF1493);
+        pink.setStyle(Paint.Style.FILL);
 
     }
 
