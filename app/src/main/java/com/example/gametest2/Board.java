5c2896438a7584906621c51ce91b2e17ee4f3971
freezy <abhiakkal27@gmail.com>
2023-03-21 09:24:12 -0700

    All action methods complete

--- app/src/main/java/com/example/gametest2/Board.java
+++ app/src/main/java/com/example/gametest2/Board.java
@@ -1,6 +1,29 @@
 package com.example.gametest2;
 
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
 public class Board extends GameView{
     //here is the board class for the basic-basic make of the game
+    Paint red = new Paint();
+    Paint blue = new Paint();
+    Paint black = new Paint();
+    Paint white = new Paint();
+    Paint dBlue = new Paint();
+    Paint pink = new Paint();
+
+    gameState gs;
+    GameModel gm;
+    public Board(Context context, AttributeSet attrs){
+        super(context, attrs);
+        gs = new gameState();
+        gm = new GameModel();
+
+        red.setColor(0xFFFF0000);
+
+    }
+
 
 }
+
--- app/src/main/java/com/example/gametest2/GameView.java
+++ app/src/main/java/com/example/gametest2/GameView.java
@@ -1,12 +1,17 @@
 package com.example.gametest2;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
 import com.example.GameFramework.Game;
 
-public class GameView {
+public class GameView extends SurfaceView{
     private GameModel gm;
 
     //constructor
-    public GameView() {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context,attrs);
         gm = new GameModel();
     }
 
@@ -14,5 +19,6 @@ public class GameView {
     public GameModel getGameModel(){
         return gm;
     }
+
 
 }
