fb8797cf9c000af48c7288fc081e9b53297b6e68
brigi <oreillyb25@up.edu>
2023-04-08 18:56:40 -0700

    makemove, some new variables, canmove, checkifgameover, sendupdatedstateto, constructor

--- app/src/main/java/com/example/gametest2/ScrabbleLocalGame.java
+++ app/src/main/java/com/example/gametest2/ScrabbleLocalGame.java
@@ -6,23 +6,66 @@ import com.example.GameFramework.players.GamePlayer;
 
 public class ScrabbleLocalGame extends LocalGame {
     ScrabbleGameState sgs;
+
+    public ScrabbleLocalGame(){
+        sgs = new ScrabbleGameState();
+    }
     @Override
     protected void sendUpdatedStateTo(GamePlayer p) {
-        sgs = new ScrabbleGameState();
+        ScrabbleGameState cp = new ScrabbleGameState(sgs);
+        p.sendInfo(cp);
     }
 
     @Override
     protected boolean canMove(int playerIdx) {
-        return true;
+        if(playerIdx == sgs.getPlayerID()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
     @Override
     protected String checkIfGameOver() {
-        return null;
+        String gameOver;
+        gameOver = "game not over";
+        if(sgs.TileCounter == 100){
+            gameOver = "game is over, identifying winner...";
+            return gameOver;
+            //need a checkScore of some sort here
+        }
+        return gameOver;
+
     }
 
     @Override
     protected boolean makeMove(GameAction action) {
-        return true;
+        int players = super.players.length;
+        if(action instanceof PlayWordAction){
+            return true;
+        }
+
+        else if(action instanceof GetNewTilesAction){
+            return true;
+        }
+
+        else if(action instanceof ShuffleAction){
+            return true;
+        }
+
+        else if(action instanceof SkipAction){
+            return true;
+        }
+
+        else if(action instanceof SpellCheckAction){
+            return true;
+        }
+
+        //return true;
+        else {
+            return false;
+        }
     }
 }
