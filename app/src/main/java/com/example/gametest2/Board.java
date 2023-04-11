2e2add7bbb73cbaa5d817ae4ebc580a014cb5cbe
freezy <abhiakkal27@gmail.com>
2023-04-10 19:39:17 -0700

    replace squares

--- app/src/main/java/com/example/gametest2/Board.java
+++ app/src/main/java/com/example/gametest2/Board.java
@@ -1,10 +1,12 @@
 package com.example.gametest2;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 
